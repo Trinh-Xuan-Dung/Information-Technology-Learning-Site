7a1f78440180e37ab0e3e37c88772f0a9f3dc894
dungtxhe141059 <dungtxhe141059@fpt.edu.vn>
2023-05-24 09:59:37 +0700

    delete old project

---
+++ src/test/java/com/example/ITLearningSite/ItLearningSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ITLearningSite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItLearningSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
