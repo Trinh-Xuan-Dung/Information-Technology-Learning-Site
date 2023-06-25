50399776c42c991446c9cee8e3805ab32bf6acbf
Nguyen Van Duan <119272062+NguyenVanDuan11@users.noreply.github.com>
2023-06-25 09:35:26 +0700

    register controller

---
+++ src/main/java/DAO/UserDAO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DAO;
+
+import Entity.Users;
+
+/**
+ *
+ * @author HP
+ */
+public interface UserDAO {
+    public int AddNewUser(Users user);
+        
+}
