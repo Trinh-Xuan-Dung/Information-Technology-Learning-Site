93eb8653800480d578e995f467b6fa08dc8e31f5
Hoannv <hoannvhe141765@fpt.edu.vn>
2023-07-18 22:18:31 +0700

    assignment dao

---
+++ src/main/java/DAO/AssignmentDAO.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package DAO;
+import Entity.Assignment;
+/**
+ *
+ * @author AAdmin
+ */
+public interface AssignmentDAO {
+    public int addNewAssignment(Assignment assigment);
+}
